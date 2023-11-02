f3d04fa0ff81deb02e436d3c3493c9794bd541d2
iiisa783 <144763655+iiisa783@users.noreply.github.com>
2023-11-02 16:30:53 +0900

    kadai_021をアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+    const newText = document.getElementById('text');
+    newText.textContent = 'ボタンをクリックしました'
+}, 2000);
+});
